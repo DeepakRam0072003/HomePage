aef18274b47c8592a699c5ef350a1324ac5873e5
DeepakRam0072003 <ramdeepak0072003@gmail.com>
2025-06-18 10:25:26 +0800

    Update HomePage.py

--- HomePage.py
+++ HomePage.py
@@ -1,5 +1,15 @@
-import streamlit as st  
-st.set_page_config(page_title="Multipage App", page_icon=":guardsman:", layout="wide")
+import streamlit as st
 
-st.title("Main Page")
-st.sidebar.success("Select a page above.")
+st.title("Application Hub")
+st.write("Navigate to different modules:")
+
+pages = {
+    "C2C_C2D": "pages/C2C_C2D.py",
+    "CANAVORP PTSPTR": "pages/CANAVORPPTSPTR.py",
+    "CANAVTO PTSPTR": "pages/CANAVTOPTSPTR.py",
+    
+}
+
+for page_name, page_path in pages.items():
+    if st.button(page_name):
+        st.switch_page(page_path)
